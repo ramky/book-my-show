f00b3dde7a9075d0d988d8fb5a0cdf29f1b60d31
Ram Iyer <ramky.iyer@gmail.com>
2021-06-03 16:13:29 -0400

    using bundle exec prefix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 # Compile the assets
-rake db:exists && rake db:migrate || rake db:setup
+bundle exec rake db:exists && bundle exec rake db:migrate || bundle exec rake db:setup
 bundle exec rake assets:precompile
 
 # Start the server
