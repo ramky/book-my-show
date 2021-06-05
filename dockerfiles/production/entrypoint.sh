ee39af320a1aa99449b12819793e31f705c4a7a8
Ram Iyer <ramky.iyer@gmail.com>
2021-06-04 23:51:16 -0400

    creating multiple dockerfiles so tests can be run with sqlite3 without prod db

--- dockerfiles/production/entrypoint.sh
+++ dockerfiles/production/entrypoint.sh
@@ -1,7 +1,3 @@
-# Run tests first
-RAILS_ENV=test bundle exec rake db:create
-RAILS_ENV=test bundle exec rspec spec
-
 # Create prod database only if it doesn't exist
 bundle exec rake db:exists && bundle exec rake db:migrate || bundle exec rake db:setup
 # Compile the assets
---
+++ dockerfiles/test/entrypoint.sh
@@ -0,0 +1,3 @@
+# Setup & run tests
+bundle exec rake db:create
+bundle exec rspec spec
